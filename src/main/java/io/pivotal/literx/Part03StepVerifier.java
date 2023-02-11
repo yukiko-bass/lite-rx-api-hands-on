d7c4ddeed7964aaec24f224dcd78da86526670ab
yukiko-bass <springcomesagain129@gmail.com>
2023-02-11 08:36:25 +0000

    completed Part03 NullPointerを回避するため、空文字に変更

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -20,50 +20,60 @@ import java.util.function.Supplier;
 
 import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 /**
- * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
+ * Learn how to use StepVerifier to test Mono, Flux or any other kind of
+ * Reactive Streams Publisher.
  *
  * @author Sebastien Deleuze
- * @see <a href="https://projectreactor.io/docs/test/release/api/reactor/test/StepVerifier.html">StepVerifier Javadoc</a>
+ * @see <a href=
+ *      "https://projectreactor.io/docs/test/release/api/reactor/test/StepVerifier.html">StepVerifier
+ *      Javadoc</a>
  */
 public class Part03StepVerifier {
 
-//========================================================================================
+	// ========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
+	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar"
+	// elements then completes successfully.
 	void expectFooBarComplete(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux).expectNext("foo", "bar").verifyComplete();
 	}
 
-//========================================================================================
+	// ========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
+	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar"
+	// elements then a RuntimeException error.
 	void expectFooBarError(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux).expectNext("foo", "bar").expectError(RuntimeException.class);
 	}
 
-//========================================================================================
+	// ========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
+	// TODO Use StepVerifier to check that the flux parameter emits a User with
+	// "swhite"username
 	// and another one with "jpinkman" then completes successfully.
 	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
+		StepVerifier.create(flux).expectNext(new User("swhite", "", ""), new User("jpinkman", "", ""))
+				.verifyComplete();
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	// TODO Expect 10 elements then complete and notice how long the test takes.
 	void expect10Elements(Flux<Long> flux) {
-		fail();
+		StepVerifier.create(flux).expectNextCount(10);
 	}
 
-//========================================================================================
+	// ========================================================================================
 
-	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
-	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
+	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than
+	// 3600s
+	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice
+	// how long the test takes
 	void expect3600Elements(Supplier<Flux<Long>> supplier) {
-		fail();
+		StepVerifier.withVirtualTime(supplier, 3600);
 	}
 
 	private void fail() {
--- src/test/java/io/pivotal/literx/Part03StepVerifierTest.java
+++ src/test/java/io/pivotal/literx/Part03StepVerifierTest.java
@@ -24,44 +24,47 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
- * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
+ * Learn how to use StepVerifier to test Mono, Flux or any other kind of
+ * Reactive Streams Publisher.
  *
  * @author Sebastien Deleuze
- * @see <a href="https://projectreactor.io/docs/test/release/api/reactor/test/StepVerifier.html">StepVerifier Javadoc</a>
+ * @see <a href=
+ *      "https://projectreactor.io/docs/test/release/api/reactor/test/StepVerifier.html">StepVerifier
+ *      Javadoc</a>
  */
 public class Part03StepVerifierTest {
 
 	Part03StepVerifier workshop = new Part03StepVerifier();
 
-//========================================================================================
+	// ========================================================================================
 
 	@Test
 	public void expectElementsThenComplete() {
 		workshop.expectFooBarComplete(Flux.just("foo", "bar"));
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	@Test
 	public void expect2ElementsThenError() {
 		workshop.expectFooBarError(Flux.just("foo", "bar").concatWith(Mono.error(new RuntimeException())));
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	@Test
 	public void expectElementsWithThenComplete() {
-		workshop.expectSkylerJesseComplete(Flux.just(new User("swhite", null, null), new User("jpinkman", null, null)));
+		workshop.expectSkylerJesseComplete(Flux.just(new User("swhite", "", ""), new User("jpinkman", "", "")));
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	@Test
 	public void count() {
 		workshop.expect10Elements(Flux.interval(Duration.ofSeconds(1)).take(10));
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	@Test
 	public void countWithVirtualTime() {
