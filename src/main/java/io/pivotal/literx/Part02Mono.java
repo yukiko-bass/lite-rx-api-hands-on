0c013e7e2fb0d84b7ca4658b338f10fbc555b691
yukiko-bass <springcomesagain129@gmail.com>
2023-02-11 08:16:05 +0000

    completed Part02

--- src/main/java/io/pivotal/literx/Part02Mono.java
+++ src/main/java/io/pivotal/literx/Part02Mono.java
@@ -6,36 +6,38 @@ import reactor.core.publisher.Mono;
  * Learn how to create Mono instances.
  *
  * @author Sebastien Deleuze
- * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html">Mono Javadoc</a>
+ * @see <a href=
+ *      "https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html">Mono
+ *      Javadoc</a>
  */
 public class Part02Mono {
 
-//========================================================================================
+	// ========================================================================================
 
 	// TODO Return an empty Mono
 	Mono<String> emptyMono() {
-		return null;
+		return Mono.empty();
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	// TODO Return a Mono that never emits any signal
 	Mono<String> monoWithNoSignal() {
-		return null;
+		return Mono.never();
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	// TODO Return a Mono that contains a "foo" value
 	Mono<String> fooMono() {
-		return null;
+		return Mono.just("foo");
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	// TODO Create a Mono that emits an IllegalStateException
 	Mono<String> errorMono() {
-		return null;
+		return Mono.error(new IllegalStateException());
 	}
 
 }
