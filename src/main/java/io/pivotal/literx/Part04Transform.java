b5d325f2048f0d55a8c847432ed690177fafe3aa
yukiko-bass <springcomesagain129@gmail.com>
2023-02-11 09:13:15 +0000

    completed Part04

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -11,29 +11,33 @@ import reactor.core.publisher.Mono;
  */
 public class Part04Transform {
 
-//========================================================================================
+	// ========================================================================================
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(u -> new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(),
+				u.getLastname().toUpperCase()));
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(u -> new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(),
+				u.getLastname().toUpperCase()));
 	}
 
-//========================================================================================
+	// ========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+	// TODO Capitalize the users username, firstName and lastName using
+	// #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(u -> asyncCapitalizeUser(u));
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
-		return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
+		return Mono.just(
+				new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
 	}
 
 }
