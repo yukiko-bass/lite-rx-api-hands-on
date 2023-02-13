50398f5ad46734cd72eec836dee6313f591662b3
yukiko-bass <springcomesagain129@gmail.com>
2023-02-13 03:22:56 +0000

    complated Part10

--- src/main/java/io/pivotal/literx/Part10ReactiveToBlocking.java
+++ src/main/java/io/pivotal/literx/Part10ReactiveToBlocking.java
@@ -11,18 +11,18 @@ import reactor.core.publisher.Mono;
  */
 public class Part10ReactiveToBlocking {
 
-//========================================================================================
+	// ========================================================================================
 
 	// TODO Return the user contained in that Mono
 	User monoToValue(Mono<User> mono) {
-		return null;
+		return mono.block();
 	}
 
-//========================================================================================
+	// ========================================================================================
 
 	// TODO Return the users contained in that Flux
 	Iterable<User> fluxToValues(Flux<User> flux) {
-		return null;
+		return flux.toIterable();
 	}
 
 }
